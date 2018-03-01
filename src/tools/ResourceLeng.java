31ba3526e5260ff5bcc1b82625dd7a4073480ca0
DIEGO <diego-A-terra@hotmail.com>
2018-03-01 13:43:45 +0100

    Testing updates

---
+++ src/tools/ResourceLeng.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ResourceLeng {
+    /** 
+     * Initial App String
+     */
+    public final static String APP_INIT = "app_init";
+    
+    /**
+     * Title on title bar
+     */
+    public final static String APP_TITLE = "app_title";
+   
+    /**
+     * Text from main button application
+     */
+    public final static String HELLO_BUTTON = "hello_button";
+    
+    /**
+     * Text for log
+     */
+    public final static String HELLO_LOG = "hello_log";
+       
+    /**
+     * Text
+     */
+    public final static String HELLO_WORLD = "hello_world";
+}
